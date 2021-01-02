12344e529b2383ae30191f11932c3f29e576baba
Aadish Samir Samir <aadish.samir@giis.edu.sg>
2021-01-02 01:48:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Don`t change any code below this line👇🏾
+######################################################
+#Function that allows for input
+
+def greet_with_name(name):
+  print(f"Hello {name}")
+  print(f"How do you do {name}?")
+######################################################
+#Don`t change any code above this line👆🏾
+
+greet_with_name("Put someone`s name here to greet them.")
